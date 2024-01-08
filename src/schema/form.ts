503dbd9ae0d52471cdd10404579deb1e1f69289b
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-01-08 23:39:34 +0900

    FEAT: add github api

--- src/schema/form.ts
+++ src/schema/form.ts
@@ -1,25 +1,9 @@
-import { z } from "zod";
-
-const castToValOrNull = <T extends Parameters<typeof z.preprocess>[1]>(
-  schema: T
-) =>
-  z.preprocess((val) => {
-    if (typeof val === "string") {
-      const trimmedVal = val.trim();
-      return trimmedVal.length > 0 ? trimmedVal : null;
-    }
-    return null;
-  }, schema);
+import { z } from 'zod';
 
 export const formSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(3, "Name must be at least 3 characters long")
-    .max(255),
-  repository: z.string().trim().min(1, "Repository is Required"),
-  file: z.string().trim().min(1, "File is Required"),
-  schedule: z.string().trim().min(1, "Schedule is Required"),
+  user: z.string().trim().min(1, 'User is Required'),
+  repository: z.string().trim().min(1, 'Repository is Required'),
+  file: z.string().trim().min(1, 'File is Required'),
 });
 
 export type FormType = z.infer<typeof formSchema>;
